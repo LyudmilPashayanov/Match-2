6764fe33be76623303b7564723ab33d500003915
Lyudmil Pashayanov <sytix1@hotmail.com>
2025-11-08 00:01:32 +0100

    menu navigation to game

--- Assets/Scripts/SceneNavigator.cs
+++ Assets/Scripts/SceneNavigator.cs
@@ -1,17 +1,40 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneNavigator : MonoBehaviour
 {
-   [SerializeField] private Button _startGameButton;
+    private const float FADE_DURATION = 0.5f;
+    
+    [SerializeField] private Button _changeSceneButton;
+    [SerializeField] private CanvasGroup _fadeCanvasGroup;
+  
+    private bool _isLoading;
+    
     private void Start()
     {
-        _startGameButton.onClick.AddListener(GoToGame);
+        _changeSceneButton.onClick.AddListener(GoToOtherScene);
     }
-
-    private void GoToGame()
+    
+    private void GoToOtherScene()
     {
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        if (_isLoading) return;
+        
+        _isLoading = true;
+
+        int goToIndex = 0;
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            goToIndex = 1;
+        }
+        
+        AsyncOperation loadOp = SceneManager.LoadSceneAsync(goToIndex);
+        loadOp.allowSceneActivation = false;
+
+        _fadeCanvasGroup.DOFade(0f, FADE_DURATION).OnComplete(() =>
+        {
+            loadOp.allowSceneActivation = true;
+        });
     }
 }
